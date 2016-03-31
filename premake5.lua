5450fc84895015e92799ead57c06dbc5d45ed315
Aria <blackstab1337@gmail.com>
2016-04-01 01:20:11 +0800

    Converted to premake

---
+++ premake5.lua
@@ -0,0 +1,15 @@
+workspace "Q3ReflexMapConverter"
+	configurations {"Debug", "Release"}
+
+project "Q3ReflexMapConverter"
+	kind "ConsoleApp"
+	language "C++"
+	characterset "MBCS"
+	targetdir "bin/%{cfg.buildcfg}"
+	buildoptions { "-std=c++11" }
+	files {"src/**.h", "src/**.hpp", "src/**.cpp"}
+	filter "configurations:Debug"
+		flags{"Symbols"}
+		defines {"_CRT_SECURE_NO_WARNINGS"}
+	filter "configurations:Release"
+		defines {"NDEBUG", "_CRT_SECURE_NO_WARNINGS"}
